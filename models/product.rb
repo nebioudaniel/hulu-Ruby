3c88d9edad95a02f17173ca92e89f4a01796f28e
Nebiou Daniel Berhane <124292845+nebioudaniel@users.noreply.github.com>
2024-04-17 21:19:19 +0300

    Update product.rb

--- models/product.rb
+++ models/product.rb
@@ -1,3 +1,11 @@
+require 'sinatra'
+require 'json'
+require 'mongo'
+
+# MongoDB setup (assuming MongoDB is already configured and running)
+Mongo::Logger.logger.level = Logger::INFO
+
+# Define Product model
 class Product
   include Mongo::Document
   include Mongo::Timestamps
@@ -10,4 +18,39 @@ class Product
 
   # Additional code goes here
   # For example, you could add methods or validations
+end
+
+# Define routes
+get '/api/products' do
+  content_type :json
+  products = Product.all
+  products.to_json
+end
+
+get '/api/products/:id' do
+  content_type :json
+  product = Product.find(params[:id])
+  product.to_json
+end
+
+post '/api/products' do
+  content_type :json
+  data = JSON.parse(request.body.read)
+  product = Product.create(data)
+  product.to_json
+end
+
+put '/api/products/:id' do
+  content_type :json
+  data = JSON.parse(request.body.read)
+  product = Product.find(params[:id])
+  product.update(data)
+  product.to_json
+end
+
+delete '/api/products/:id' do
+  content_type :json
+  product = Product.find(params[:id])
+  product.delete
+  { message: 'Product deleted successfully' }.to_json
 end
